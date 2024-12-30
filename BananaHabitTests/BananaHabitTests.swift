8b49ed22f37062b97ab588c90469b35c2758c146
01441958 <shaowenxuan@sf-express.com>
2024-12-30 17:00:34 +0800

    Initial Commit

---
+++ BananaHabitTests/BananaHabitTests.swift
@@ -0,0 +1,16 @@
+//
+//  BananaHabitTests.swift
+//  BananaHabitTests
+//
+//  Created by 邵文萱(ShaoWenxuan)-顺丰科技技术集团 on 2024/12/30.
+//
+
+import Testing
+
+struct BananaHabitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
